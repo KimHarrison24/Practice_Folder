ea72e0d262b0641d77909fe2ac46b80e1cc24ab3
KimHarrison24 <christianharris.garcia.24@gmail.com>
2023-02-08 14:19:50 +0800

    Added Files

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+//var greetMessage = "Ohayou Sekai, I'm External Java Script!";
+
+//var x = 5;
+//var y = 10;
+
+//var product = x * y;
+
+//document.write(product);
+
+//var name = "Shirra Kimberly", age = 22, isMarried = false;
+
+//document.write("Name: " + name + ",  Age: " + age + ", Married: " + isMarried);
+
+//const PI = 3.14;
+//const PI = 5.14;
+
+//console.log(PI);
+
+var a = "Let's have a cup of Coffee";
+var b = "We said 'Hello' and left.";
+var c = "'I'll', 'We' never Give Up!";
+
+document.write(a + "<br>" + b + "<br>" + c);
