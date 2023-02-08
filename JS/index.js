4a9d93454295ae3bf12225d08ff7118565ec9cd0
KimHarrison24 <christianharris.garcia.24@gmail.com>
2023-02-08 15:26:31 +0800

    Added Files

--- JS/index.js
+++ JS/index.js
@@ -16,8 +16,38 @@
 
 //console.log(PI);
 
+/*
 var a = "Let's have a cup of Coffee";
 var b = "We said 'Hello' and left.";
 var c = "'I'll', 'We' never Give Up!";
 
 document.write(a + "<br>" + b + "<br>" + c);
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+var a = 25;
+var b = 80.5;
+var c = 4.25e6;
+var d = 4.25e-6;
+
+document.write(a + "<br>" + b + "<br>" + c + "<br>"+ d);
+
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+
+
+var me = "'First Name: Chistian Harris, Middle Name: Fernandez, Surname: Garcia' <br> 'First Name: Chistian Harris, Middle Name: Fernandez, Surname: Garcia'";
+
+document.write(me);
+*/
+
+var total_priceDisplay = function(){
+
+    var total_priceDollars = document.getElementById("pieces").value * 2;
+
+    return alert("Total Price of Item: $" +total_priceDollars);
+}
