cad0cd1b1e6d2027604af1855cf62810d8bc173b
KimHarrison24 <christianharris.garcia.24@gmail.com>
2023-02-15 11:16:37 +0800

    Added Files

---
+++ JS/contextmenu.js
@@ -0,0 +1,21 @@
+/*
+function message() 
+{
+    alert("You have Right Click a Paragraph!");
+}
+
+function message1() 
+{
+    alert("You have Right Click a Link!");
+}
+*/
+
+function message() 
+{
+    alert("You have Move out to Paragraph!");
+}
+
+function message1() 
+{
+    alert("You have Move out to Link!");
+}
