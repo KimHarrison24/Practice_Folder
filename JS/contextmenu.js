631cf4f31d9b18fa4b25a2148b33487fe689ef75
KimHarrison24 <christianharris.garcia.24@gmail.com>
2023-02-15 13:45:09 +0800

    Added Files

--- JS/contextmenu.js
+++ JS/contextmenu.js
@@ -44,15 +44,3 @@ function colorChange()
 {
     document.getElementById("this").style.backgroundColor = "Yellow";
 }
-
-function lightsOff()
-{
-    document.getElementById("off").style.display = "block";
-    document.getElementById("on").style.display = "none";
-}
-
-function lightsOn()
-{
-    document.getElementById("on").style.display = "block";
-    document.getElementById("off").style.display = "none";
-}
---
+++ JS/lightbulb.js
@@ -0,0 +1,11 @@
+function lightsOff()
+{
+    document.getElementById("off").style.display = "block";
+    document.getElementById("on").style.display = "none";
+}
+
+function lightsOn()
+{
+    document.getElementById("on").style.display = "block";
+    document.getElementById("off").style.display = "none";
+}
