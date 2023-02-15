c82e585902b55b050b2490e1a2df35b57ee149bf
KimHarrison24 <christianharris.garcia.24@gmail.com>
2023-02-15 15:46:14 +0800

    Added Files

--- JS/contextmenu.js
+++ JS/contextmenu.js
@@ -38,9 +38,70 @@ function message4()
 function submitMessage() 
 {
     alert("Form Submitted!");
+    document.getElementById("form").style.display = "none";
+}
+function loadMessage() 
+{
+    document.getElementById("form").style.display = "block";
 }
 
 function colorChange() 
 {
     document.getElementById("this").style.backgroundColor = "Yellow";
+}
+
+/*
+function windowResize() {
+    var w = window.outerWidth;
+    var h = window.outerHeight;
+    //var txt = "window size: width" + w + ", height: " + h;
+
+    document.getElementById("here").style.width = w;
+}
+window.onresize = windowResize;
+*/
+
+function dailyMessage() {
+    var now = new Date();
+    var dateOfTheweek = now.getDay();
+
+    if(dateOfTheweek == 0)
+    {
+        alert("Have a Bless Sunday!!!");
+    }
+
+    else if(dateOfTheweek == 1)
+    {
+        alert("Hayss, Monday na Naman!!!");
+    }
+
+    else if(dateOfTheweek == 2)
+    {
+        alert("Have a Good Tuesday!!!");
+    }
+
+    else if(dateOfTheweek == 3)
+    {
+        alert("Have a Beautiful day as Wednesday!!!");
+    }
+
+    else if(dateOfTheweek == 4)
+    {
+        alert("Have a Good Thursday!!!");
+    }
+
+    else if(dateOfTheweek == 5)
+    {
+        alert("Yes, Thank Goodness its Friday!!!");
+    }
+
+    else if(dateOfTheweek == 6)
+    {
+        alert("Saturday? Anime Day!!!");
+    }
+
+    else
+    {
+        alert("Have a Good Day!!!");
+    }
 }
